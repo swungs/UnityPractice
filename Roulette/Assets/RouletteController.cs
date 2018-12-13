8605bebd4e3da8afa8118313018fee24768f56f8
swungs <45559819+swungs@users.noreply.github.com>
2018-12-13 23:00:15 +0900

    작업물 업로드#2
    빼먹은 2개 더

---
+++ Roulette/Assets/RouletteController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour {
+
+    float rotSpeed = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        //클릭하면 회전 속도를 설정한다.
+        if (Input.GetMouseButtonDown(0))
+        {
+            this.rotSpeed = 20;
+        }
+
+        //회전 속도만큼 룰렛을 회전시킨다.
+        transform.Rotate(0, 0, rotSpeed);
+
+        //룰렛을 감속시킨다.
+        this.rotSpeed *= 0.98f;
+
+	}
+}
---
+++ SwipeCar/Assets/GameDirector.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour {
+
+    GameObject car;
+    GameObject flag;
+    GameObject distance;
+
+	// Use this for initialization
+	void Start () {
+        this.car = GameObject.Find("car");
+        this.flag = GameObject.Find("flag");
+        this.distance = GameObject.Find("Distance");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float length = this.flag.transform.position.x - this.car.transform.position.x;
+        string length_string = length.ToString("F2");
+
+        if (length > 0)
+        {
+            this.distance.GetComponent<Text>().text = "목표 지점까지 " + length_string + " m";
+        }
+        else
+        {
+            this.distance.GetComponent<Text>().text = "게임 오버";
+        }
+        
+	}
+}
